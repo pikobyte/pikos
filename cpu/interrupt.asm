d7f910a157a1eb9ea15e641f6ea674200a9d471c
pikobyte <anthony.mercer@hotmail.co.uk>
2023-04-10 13:03:08 +0000

    Push register ptr into ESP

--- cpu/interrupt.asm
+++ cpu/interrupt.asm
@@ -11,43 +11,46 @@
 
 ; Common ISR function.
 isr_common:
-	  pusha            ; Pushes general purpose registers
-	  mov   ax, ds     ; Move DS into the lower 16-bits of EAX
-	  push  eax        ; Save the data segment descriptor
-	  mov   ax, 0x10   ; Copy the kernel data segment descriptor into AX
-	  mov   ds, ax     ; and then to all segment registers
-	  mov   es, ax
-	  mov   fs, ax
-	  mov   gs, ax
+	pusha               ; Pushes general purpose registers
+	mov   ax, ds        ; Move DS into the lower 16-bits of EAX
+	push  eax           ; Save the data segment descriptor
+	mov   ax, 0x10      ; Copy the kernel data segment descriptor into AX
+	mov   ds, ax        ; and then to all segment registers
+	mov   es, ax
+	mov   fs, ax
+	mov   gs, ax
+    push  esp           ; Push the register pointer
 	
     cld                 ; Clear the direction flag
-	  call isr_handler    ; Call the C ISR handling function
+	call isr_handler    ; Call the C ISR handling function
 	
-	  pop   eax           ; Restore the general purpose registers
-	  mov   ds, ax
-	  mov   es, ax
-	  mov   fs, ax
-	  mov   gs, ax        ; Restore the segment registers
-	  popa
-	  add   esp, 8        ; Clean up the pushed error code and pushed ISR number
-	  sti                 ; Set interrupt flag
-	  iret                ; Interrupt return, pops CS, EIP, EFLAGS, SS, and ESP
+    pop   eax           ; Restore ESP
+	pop   eax           ; Restore the general purpose registers
+	mov   ds, ax
+	mov   es, ax
+	mov   fs, ax
+	mov   gs, ax        ; Restore the segment registers
+	popa
+	add   esp, 8        ; Clean up the pushed error code and pushed ISR number
+	iret                ; Interrupt return, pops CS, EIP, EFLAGS, SS, and ESP
 
 
 ;  Common IRQ function.
   irq_common:
     pusha               ; Pushes general purpose registers
-	  mov   ax, ds        ; Move DS into the lower 16-bits of EAX
-	  push  eax           ; Save the data segment descriptor
-	  mov   ax, 0x10      ; Copy the kernel data segment descriptor into AX
-	  mov   ds, ax        ; and then to all segment registers
-	  mov   es, ax
-	  mov   fs, ax
-	  mov   gs, ax
+	mov   ax, ds        ; Move DS into the lower 16-bits of EAX
+	push  eax           ; Save the data segment descriptor
+	mov   ax, 0x10      ; Copy the kernel data segment descriptor into AX
+	mov   ds, ax        ; and then to all segment registers
+	mov   es, ax
+	mov   fs, ax
+	mov   gs, ax
+    push  esp           ; Push the register pointer
 
     cld                 ; Clear the direction flag
     call  irq_handler   ; Call the C ISQ handling function
 
+    pop   ebx           ; Restore ESP
     pop   ebx           ; Restore the general purpose registers
     mov   ds, bx
     mov   es, bx
@@ -55,7 +58,6 @@ isr_common:
     mov   gs, bx        ; Set the segment registers to EBX
     popa
     add   esp, 8        ; Clean up the pushed error code and pushed ISR number
-    sti                 ; Set interrupt flag
     iret                ; Interrupt return, pops CS, EIP, EFLAGS, SS, and ESP
 
 ; Define the C handling functions for the interrupt services registers and the
@@ -117,218 +119,186 @@ global irq15
 
 ; ISR handlers.
 isr0: ; Divide-by-zero exception
-    cli
     push  byte 0
     push  byte 0
     jmp   isr_common
 
 
 isr1: ; Debug exception
-    cli
     push  byte 0
     push  byte 1
     jmp   isr_common
 
 
 isr2: ; Non-maskable interrupt exception
-    cli
     push  byte 0
     push  byte 2
     jmp   isr_common
 
 
 isr3: ; Int 3 exception
-    cli
     push  byte 0
     push  byte 3
     jmp   isr_common
 
 
 isr4: ; INTO exception
-    cli
     push  byte 0
     push  byte 4
     jmp   isr_common
 
 
 isr5: ; Out-of-bounds exception
-    cli
     push  byte 0
     push  byte 5
     jmp   isr_common
 
 
 isr6: ; Invalid opcode exception
-    cli
     push  byte 0
     push  byte 6
     jmp   isr_common
 
 
 isr7: ; Co-processor not available exception
-    cli
     push  byte 0
     push  byte 7
     jmp   isr_common
 
 
 isr8: ; Double fault exception /w error code
-    cli
     push byte 8
     jmp isr_common
 
 
 isr9: ; Co-processor segment overrun exception
-    cli
     push  byte 0
     push  byte 9
     jmp   isr_common
 
 
 isr10: ; Bad TSS exception \w error code
-    cli
     push byte 10
     jmp isr_common
 
 
 isr11: ; Segment not present exception \w error code
-    cli
     push byte 11
     jmp isr_common
 
 
 isr12: ; Stack fault exception \w error code
-    cli
     push byte 12
     jmp isr_common
 
 
 isr13: ; General protection fault exception \w error code
-    cli
     push byte 13
     jmp isr_common
 
 
 isr14: ; Page fault exception
-    cli
     push byte 14
     jmp isr_common
 
 
 isr15: ; Reserved exception
-    cli
     push  byte 0
     push  byte 15
     jmp   isr_common
 
 
 isr16: ; Floating point exception
-    cli
     push  byte 0
     push  byte 16
     jmp   isr_common
 
 
 isr17: ; Aligment check exception
-    cli
     push  byte 0
     push  byte 17
     jmp   isr_common
 
 
 isr18: ; Machine check exception
-    cli
     push  byte 0
     push  byte 18
     jmp   isr_common
 
 
 isr19: ; Reserved
-    cli
     push  byte 0
     push  byte 19
     jmp   isr_common
 
 
 isr20: ; Reserved
-    cli
     push  byte 0
     push  byte 20
     jmp   isr_common
 
 
 isr21: ; Reserved
-    cli
     push  byte 0
     push  byte 21
     jmp   isr_common
 
 
 isr22: ; Reserved
-    cli
     push  byte 0
     push  byte 22
     jmp   isr_common
 
 
 isr23: ; Reserved
-    cli
     push  byte 0
     push  byte 23
     jmp   isr_common
 
 
 isr24: ; Reserved
-    cli
     push  byte 0
     push  byte 24
     jmp   isr_common
 
 
 isr25: ; Reserved
-    cli
     push  byte 0
     push  byte 25
     jmp   isr_common
 
 
 isr26: ; Reserved
-    cli
     push  byte 0
     push  byte 26
     jmp   isr_common
 
 
 isr27: ; Reserved
-    cli
     push  byte 0
     push  byte 27
     jmp   isr_common
 
 
 isr28: ; Reserved
-    cli
     push  byte 0
     push  byte 28
     jmp   isr_common
 
 
 isr29: ; Reserved
-    cli
     push  byte 0
     push  byte 29
     jmp   isr_common
 
 
 isr30: ; Reserved
-    cli
     push  byte 0
     push  byte 30
     jmp   isr_common
 
 
 isr31: ; Reserved
-    cli
     push  byte 0
     push  byte 31
     jmp   isr_common
